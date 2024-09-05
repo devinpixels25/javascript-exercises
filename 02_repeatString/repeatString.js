bbe9cca847917dbb5e9fe89f546abdf5bc172604
devin <devinpixels25@gmail.com>
2024-09-04 23:07:10 -0400

    pass 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  stringTwo = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (i = 0; i < num; i++) {
+    stringTwo += str;
+  }
+  return stringTwo;
 };
-
+repeatString("goodbye", -1);
 // Do not edit below this line
 module.exports = repeatString;
